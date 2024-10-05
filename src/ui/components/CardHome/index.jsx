242e96c14f7f089cc862f6f55a093e87a2468782
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2024-10-04 23:03:10 -0300

    Correções na Home

--- src/ui/components/CardHome/index.jsx
+++ src/ui/components/CardHome/index.jsx
@@ -3,19 +3,19 @@ import './styles.css'
 import { Link } from 'react-router-dom'
 
 export function CardHome({ title, image, externalLink, link }) {
-  if (externalLink) {
+  if (!externalLink) {
     return (
-      <Link className="function-items" to={link}>
         <div className="function-items">
           <div className="text-container">
             <img className="items-img" src={image} alt={title} />
             <p className="items-text">{title}</p>
           </div>
-          <button className="button-icon">
-            <CaretRight size={30} color="#fff" />
-          </button>
+          <Link to={link}>
+            <button className="button-icon">
+              <CaretRight size={30} color="#fff" />
+            </button>
+          </Link>
         </div>
-      </Link>
     )
   }
 
@@ -26,7 +26,7 @@ export function CardHome({ title, image, externalLink, link }) {
         <p className="items-text">{title}</p>
       </div>
 
-      <a href={link} className="a-container" target="_blank" rel="noreferrer">
+      <a href={link} target="_blank" rel="noreferrer">
         <button className="button-icon">
           <CaretRight size={30} color="#fff" />
         </button>
